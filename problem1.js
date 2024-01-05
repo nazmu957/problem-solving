d8f1eb464b488410ee83b4a86fa7f69bf6a79c36
nazmu957 <nazmus15-1999@diu.edu.bd>
2024-01-05 15:41:11 +0600

    solved 1b+c

--- problem1.js
+++ problem1.js
@@ -1,13 +1,36 @@
-// Const arr  = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9,  13];
+//Problem1: Const arr  = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9,  13];
 
-// How to remove duplicate elements from this array?
 
-// How to find out the largest/lowest elements from this array?
 
-// How to slice(22, 48) from this array
+//Problem1a: How to remove duplicate elements from this array?
+// const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
 
-const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
+// const uniqueArr = [...new Set(arr)];
 
-const uniqueArr = [...new Set(arr)];
+// console.log(uniqueArr);
 
-console.log(uniqueArr);
+
+
+
+//Problem1b: How to find out the largest/lowest elements from this array?
+// const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
+
+// const maxElement = Math.max(...arr);
+// const minElement = Math.min(...arr);
+
+// console.log("Largest Element:", maxElement);
+// console.log("Lowest Element:", minElement);
+
+
+
+//Problem1c: How to slice(22, 48) from this array
+// const arr = [1, 5, 9, 14, 5, 22, 48, 25, 22, 20, 9, 13];
+
+
+// const startIndex = arr.indexOf(22);
+// const endIndex = arr.indexOf(48) + 1; // Adding 1 to include the element at endIndex
+
+
+// const subArray = arr.slice(startIndex, endIndex);
+
+// console.log(subArray);
